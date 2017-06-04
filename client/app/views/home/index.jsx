186c1d93a2366a0c8dbcd353085fbeedde115048
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-06-04 17:15:53 -0400

    Using ramda

--- client/app/views/home/index.jsx
+++ client/app/views/home/index.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Poll from "../../poll";
+import R from "ramda";
+
+const POLLS_PER_ROW = 3;
+
+const renderPolls = R.map(poll => (<Poll {...poll} />));
+
+const renderRows = R.map(row => (
+    <div className="row">
+        {renderPolls(row)}
+    </div>
+));
 
 class HomeView extends React.Component {
     render() {
+        const rows = R.splitEvery(POLLS_PER_ROW, this.props.polls);
+
         return (
             <div className="container-fluid">
-                {this.props.polls.map(function(poll, i) {
-                    return <Poll key={i} {...poll} />
-                })}
+                {renderRows(rows)}
             </div>
         );
     }
