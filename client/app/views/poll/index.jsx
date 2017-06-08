559ec27c46b89c453afc335855ef75910327b530
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-06-08 06:55:58 -0400

    Created side cards in poll view

--- client/app/views/poll/index.jsx
+++ client/app/views/poll/index.jsx
@@ -6,9 +6,11 @@ class PollView extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <div class="row">
-                    <div className="col-xs-2"></div>
-                    <div className="col-xs-9">
+                <div className="row">
+                    <div className="col-xs-2">
+                        <div className="side-card left"></div>
+                    </div>
+                    <div className="col-xs-8">
                         <div className="col-xs-12 poll-view">
                             <div className="row">
                                 <div className="col-xs-1">
@@ -29,10 +31,17 @@ class PollView extends React.Component {
                                     <PollOption count={7} totalVotes={10} name="Coke" />
                                     <PollOption count={3} totalVotes={10} name="Pepsi" />
                                 </div>
+                                <div className="row">
+                                    <a className="poll-tags" href="#">#myamazingtag</a>
+                                    <a className="poll-tags" href="#">#anothertag</a>
+                                    <a className="poll-tags" href="#">#supertag</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-xs-2"></div>
+                    <div className="col-xs-2">
+                        <div className="side-card right"></div>
+                    </div>
                 </div>
             </div>
         )
