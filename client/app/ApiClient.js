66b13da6b26cdee294c964d4a808dedfcedb7fda
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-07-30 22:07:51 -0400

    Added new endpoints to api client

--- client/app/ApiClient.js
+++ client/app/ApiClient.js
@@ -6,7 +6,11 @@ const callbackTransform = next => ((err, res) => {
 });
 
 const get = uri => (next => request.get(uri).end(callbackTransform(next)));
+const post = (uri, body) => (next => request.post(uri).send(body).end(next));
 
 export const getPolls = get("/api/polls");
 export const getTrending = get("/api/polls/trending");
 export const getPoll = (id, next) => get(`/api/polls/${id}`)(next);
+
+export const createUser = (email, password) => post("/api/users/create", { email, password });
+export const login = (email, password) => post("/api/users/login", { email, password });
