dd6ff18b82612405db2417bce30ed929ed361ca8
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-08-08 06:46:17 -0400

    Switched to environment variables for configuration

--- server/friendly-guacamole-server/src/main/scala/ca/friendlyguacamole/server/Quill.scala
+++ server/friendly-guacamole-server/src/main/scala/ca/friendlyguacamole/server/Quill.scala
@@ -1,10 +1,29 @@
 package ca.friendlyguacamole.server
 
+import com.typesafe.config.ConfigValueFactory
+import io.getquill.util.LoadConfig
 import io.getquill.{PostgresAsyncContext, SnakeCase}
 
 /**
   * Created by panagiotis on 30/07/17.
   */
 object Quill {
-  val ctx: PostgresAsyncContext[SnakeCase] = new PostgresAsyncContext[SnakeCase]("ctx")
+  private val DbNameKey = "database"
+  private val DbHostKey = "host"
+  private val DbUserKey = "user"
+  private val DbPasswordKey = "password"
+
+  private val baseConfig = LoadConfig("ctx")
+  private val dbName = sys.env.getOrElse("GUAC_DB_NAME", baseConfig.getString(DbNameKey))
+  private val dbHost = sys.env.getOrElse("GUAC_DB_HOST_NAME", baseConfig.getString(DbHostKey))
+  private val dbUser = sys.env.getOrElse("GUAC_DB_USER", baseConfig.getString(DbUserKey))
+  private val dbPasswordKey = sys.env.getOrElse("GUAC_DB_PASSWORD", baseConfig.getString(DbPasswordKey))
+
+  val ctx: PostgresAsyncContext[SnakeCase] = new PostgresAsyncContext[SnakeCase](
+    baseConfig
+      .withValue(DbNameKey, ConfigValueFactory.fromAnyRef(dbName))
+      .withValue(DbHostKey, ConfigValueFactory.fromAnyRef(dbHost))
+      .withValue(DbUserKey, ConfigValueFactory.fromAnyRef(dbUser))
+      .withValue(DbPasswordKey, ConfigValueFactory.fromAnyRef(dbPasswordKey))
+  )
 }
--- server/friendly-guacamole-server/src/main/scala/ca/friendlyguacamole/server/util/Token.scala
+++ server/friendly-guacamole-server/src/main/scala/ca/friendlyguacamole/server/util/Token.scala
@@ -9,7 +9,7 @@ import scala.util.Try
   */
 object Token {
   private val TAlgorithm = JwtAlgorithm.HS512
-  private val TSecret = "hdeP*H#*RNuekjdsnx"
+  private val TSecret = sys.env.getOrElse("GUAC_TOKEN_SECRET", "hdeP*H#*RNuekjdsnx")
   private val TExpirySeconds = 60 * 4
 
   def toToken(userId: Int): String = {
